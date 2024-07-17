0e8f479be23cf6daae2f4295e1cb8aa3bcc299b1
sssjaajkj <vshiva4646@gmail.com>
2024-07-18 04:00:17 +0530

    varibales

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "Hello from 02-varible"
-echo "Ramesh:"" Hello Suresh , how are you"
-
-echo "Suresh:" i m fine Ramesh , how are you"
+echo "Ramesh :  Hello Suresh , how are you"
+echo "Suresh: i m fine Ramesh , how are you"
