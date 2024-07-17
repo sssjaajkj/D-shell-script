8e976e8d8c0ea278d85b91e50d74bdac36c1999b
sssjaajkj <vshiva4646@gmail.com>
2024-07-18 04:31:29 +0530

    05

---
+++ 05-varibles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Please enter username::"
+
+read USERNAME  # Here USERNAME is variable
+
+
+echo "Please enter password::"
+read PASSWORD
+
+ ech "username is $USERNAME and password is $PASSWORD"
