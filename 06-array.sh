e697919e66d5376fae334b252a7a02b01a61bc4f
sssjaajkj <vshiva4646@gmail.com>
2024-07-18 04:54:21 +0530

    05

--- 06-array.sh
+++ 06-array.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-MOVIES=("RRR" "KGF" "DJ3") # LIST OF ELEMEMT
+# array of element
+MOVIES=("RRR" "KGF" "DJ3") # LIST OF ELEMEMT no comas , only space
 
 echo "First Movies is : ${MOVIES[0]}"
-echo "First Movies is : ${MOVIES[@]}"
+echo "First Movies is : ${MOVIES[@]}" # all the values @ for everyting
