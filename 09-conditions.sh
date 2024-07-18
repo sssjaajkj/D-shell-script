b55ed0a54f927be1a4447a83b79289dfc4f7dcec
sssjaajkj <vshiva4646@gmail.com>
2024-07-18 12:35:23 +0530

    special

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,9 +2,9 @@
 num=$1
 if [$num -gt 10]
 then
-   echo " Gien number is $num is gt 10"
+   echo " Given number is $num is gt 10"
 else
-   echo " Gien number is $num is lt 10"
+   echo " Given number is $num is lt 10"
 fi
 
 # -gt -lt -eq -ge -le
