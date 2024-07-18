67a717cb236bcb3585f7e7194af936963174bcde
sssjaajkj <vshiva4646@gmail.com>
2024-07-18 12:57:55 +0530

    special

--- 10-insatllation.sh
+++ 10-insatllation.sh
@@ -28,6 +28,7 @@ echo "userid is: :$USERID"
 if [ $USERID -ne 0 ]
 then 
     echo "Please run this script with root access"
+    exit 1 # manually exit if error comes
 else
 echo "You are super usser..."
 fi
