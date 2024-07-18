a19ff06e4ddb3e9581495fc8ce5481f869265054
sssjaajkj <vshiva4646@gmail.com>
2024-07-18 19:03:10 +0530

    funation

---
+++ 15-insatll-packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+USERID=($ id -u)
+if [ $USERID -ne 0 ]
+then 
+ echo  "plz run this script with root access....."
+else
+    echo "You are super user"
+fi
+echo "All packages : $@"    
