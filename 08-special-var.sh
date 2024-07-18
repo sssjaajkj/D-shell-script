0bd509ba3d1853c980eb4c4a8b6fce34bc235358
sssjaajkj <vshiva4646@gmail.com>
2024-07-18 11:06:21 +0530

    special

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -2,3 +2,12 @@
 
 echo "All variables : $@"
 echo "Number of variable passed : $#"
+echo "script name : $0"
+echo "Current working directory : $PWD"
+echo "Home directory of current user : $HOME"
+echo "Which user is runing this script : $USER"
+echo "Hostname : $HOSTNAME"
+echo "PROCESS ID OF THE CURRENT SHELL SCRIPT : $$"
+sleep 60 &
+echo "Process ID of last background command :$!"
+
