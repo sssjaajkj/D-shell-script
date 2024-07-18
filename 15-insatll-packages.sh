b72f017a6363a433216ed7e3d7a3bab9a6571b5e
sssjaajkj <vshiva4646@gmail.com>
2024-07-18 19:20:14 +0530

    funation

--- 15-insatll-packages.sh
+++ 15-insatll-packages.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "."  -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 if [ $USERID -ne 0 ]
 then 
  echo  "plz run this script with root access....."
@@ -12,4 +15,12 @@ fi
 for i in $@
 do
     echo "package to install $i"
+    dnf list installed $i &>>$LOGFILE
+    if [$? -eq 0 ]
+
+    then 
+    echo "$i already installed .....SKIPPING 
+    else
+        echo "$i not installed .....install 
+
 done  
