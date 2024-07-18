f572208d642ffca79108c221cb67bd32b20b0c88
sssjaajkj <vshiva4646@gmail.com>
2024-07-18 17:04:41 +0530

    funation

--- 11-functions.sh
+++ 11-functions.sh
@@ -5,8 +5,13 @@ echo "userid is: :$USERID"
 
 # fun declaration
 VALIDATE(){
- echo "Exit status :$1"
- echo "What are you doing: $2"
+
+if  [ $1 -ne 0 ]
+then 
+    echo "$2 ... Failure"
+    exit
+     echo "$2 ... Success"
+
 
 }
 if [ $USERID -ne 0 ]
@@ -14,7 +19,7 @@ then
     echo "Please run this script with root access"
     exit 1 # manually exit if error comes
 else
-echo "You are super usser..."
+echo "You are super user..."
 fi
 dnf install mysql -y
 
