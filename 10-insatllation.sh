a1de2df8f7b18e70c794bc753b07718d1f282b37
sssjaajkj <vshiva4646@gmail.com>
2024-07-18 12:35:36 +0530

    special

---
+++ 10-insatllation.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Installation of something
+#—----------------------------
+# If want to install you should have super user access
+# so check user’ 
+# If super user proced = id is zero 0
+# If not super user , i should throuw proper error megs
+
+#HOw you run a command inside shell script and take the output?
+# if id is 0 then super user
+# else not a super user
+
+#[ ec2-user@ip-172-31-95-217 ~ ]$ id
+#uid=1001(ec2-user) gid=1001(ec2-user) groups=1001(ec2-user)
+
+# [ ec2-user@ip-172-31-95-217 ~ ]$ sudo su
+
+# 35.170.187.9 | 172.31.95.217 | t2.micro | null
+# [ root@ip-172-31-95-217 /home/ec2-user ]# id 
+# uid=0(root) gid=0(root) groups=0(root)
+#[ root@ip-172-31-95-217 /home/ec2-user ]# id -u
+ #0
+
+
+USERID=$(id -u)
+if[$USERID -ne 0]
+then 
+    echo "Please run this script with root access"
+else
+echo "You are super usser..."
+fi
+dnf install mysql -y
