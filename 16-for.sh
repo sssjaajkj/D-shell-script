1d3ff630aa010dcee2ae31c29cbb65f391ea29a5
sssjaajkj <vshiva4646@gmail.com>
2024-07-19 13:06:13 +0530

    install

--- 16-for.sh
+++ 16-for.sh
@@ -3,8 +3,12 @@ USERID=$(id -u)
 
 echo "Userid is: $USERID"
 
-
-
+if [ $USERID -eq  0 ]
+    then
+    echo "this root user: $USERID"
+    else
+    echo "this not root user: $USERID"
+fi
 
 # for i in {110..20}
 # do 
