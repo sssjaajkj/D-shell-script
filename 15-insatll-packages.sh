2d00ff855fa7f773790c292a3b8db9f289e3093f
sssjaajkj <vshiva4646@gmail.com>
2024-07-19 12:49:13 +0530

    install

--- 15-insatll-packages.sh
+++ 15-insatll-packages.sh
@@ -4,6 +4,10 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "."  -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 VALIDATE(){
 if  [ $1 -ne 0 ]
 then 
@@ -29,9 +33,9 @@ do
     dnf list installed $i &>>$LOGFILE
     if [ $? -eq 0  ]
     then 
-         echo "$i already installed .....SKIPPING"
+         echo -e "$i already installed .....$Y SKIPPING $N"
     else
 
-        echo "$i not installed .....install"
+        echo  -e "$i not installed .....$R install $N"
 fi
 done  
