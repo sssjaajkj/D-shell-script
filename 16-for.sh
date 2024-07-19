d0155f866839fb9bb71069747a164bf78b5b99de
sssjaajkj <vshiva4646@gmail.com>
2024-07-19 13:08:06 +0530

    install

--- 16-for.sh
+++ 16-for.sh
@@ -6,7 +6,7 @@ echo "Userid is: $USERID"
 if [ $USERID -eq  0 ]
     then
     echo "this root user: $USERID"
-    dnf install mysqld -y
+    dnf install mysql -y
     else
     echo "this not root user: $USERID"
 fi
